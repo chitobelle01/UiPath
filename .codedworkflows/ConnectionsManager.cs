3f07e303b27624a3a513ac8dafc7280d860c94d6
Rose Janine <rose.janine.pasumala@accenture.com>
2024-11-29 11:48:57 +0800

    Initial Commit for this Project - PLDTSMART SIM Registration

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace PLDTSMARTSIMRegistration
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
---
+++ Archive/SR_RG_001.cs
@@ -0,0 +1,34 @@
+using PLDTSMARTSIMRegistration.ObjectRepository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Excel;
+using UiPath.Excel.Activities;
+using UiPath.Excel.Activities.API;
+using UiPath.Excel.Activities.API.Models;
+using UiPath.Mail.Activities.Api;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.Testing;
+using UiPath.Testing.Activities.Api.Models;
+using UiPath.Testing.Activities.Models;
+using UiPath.Testing.Activities.TestData;
+using UiPath.Testing.Activities.TestDataQueues.Enums;
+using UiPath.Testing.Enums;
+using UiPath.UIAutomationNext.API.Contracts;
+using UiPath.UIAutomationNext.API.Models;
+using UiPath.UIAutomationNext.Enums;
+
+namespace PLDTSMARTSIMRegistration
+{
+    public class SR_RG_001 : CodedWorkflow
+    {
+        [TestCase]
+        public void Execute()
+        {
+
+        }
+    }
+}
